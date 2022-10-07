abf41bd46bd9ede32c882d4a521a7194f3ad5c53
Safa <safa.89.saoudi@gmail.com>
2022-10-07 14:32:13 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products', component:ProductsListComponent},
+  {path:'users', component:UsersListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsListComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
