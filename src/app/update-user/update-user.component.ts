9d91bd152300c750eafad268c7224dc32d36c78c
Safa <safa.89.saoudi@gmail.com>
2022-10-14 16:40:00 +0100

    'lazyLoading'

--- src/app/update-user/update-user.component.ts
+++ src/app/update-user/update-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-user',
@@ -7,13 +7,15 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./update-user.component.css']
 })
 export class UpdateUserComponent implements OnInit {
-myId!:Number;
-  constructor(private actR: ActivatedRoute) { }
+myId=0;
+  constructor(private actR: ActivatedRoute, private R:Router) { }
 
   ngOnInit(): void {
-    //this.myId = Number(this.actR.snapshot.params['param']);
+   //this.myId = Number(this.actR.snapshot.params['param']);
     this.actR.paramMap.subscribe(data => this.myId = Number(data.get('param')));
-
   }
+  // updateNext(){
+  //   this.R.navigate(['updateUser', this.myId++]);
+  // }
 
 }
