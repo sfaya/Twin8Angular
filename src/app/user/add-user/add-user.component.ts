2d0763bfd579a39adf40a566a71064a990dca44c
Safa <safa.89.saoudi@gmail.com>
2022-10-28 16:36:28 +0100

    reactiveForm

--- src/app/user/add-user/add-user.component.ts
+++ src/app/user/add-user/add-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { User } from 'src/app/Models/user';
 
 @Component({
@@ -9,17 +9,29 @@ import { User } from 'src/app/Models/user';
 })
 export class AddUserComponent implements OnInit {
 
-reactiveForm = new FormGroup({
-firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
-lastName: new FormControl(''),
-credentials: new FormGroup({
-  email: new FormControl('', Validators.email),
-  password: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9]{8,}?')])
-})
+// reactiveForm = new FormGroup({
+// firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+// lastName: new FormControl(''),
+// credentials: new FormGroup({
+//   email: new FormControl('', Validators.email),
+//   password: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9]{8,}?')])
+// })
 
+//});
+
+reactiveForm = this.fb.group({
+firstName:['', [Validators.required, Validators.minLength(3)]],
+lastName: [''],
+credentials : this.fb.group({
+email: ['', Validators.email],
+password: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9]{8,}?')])]}),
+skills: this.fb.array([])
 });
 
-  constructor() { }
+
+
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
 
@@ -27,11 +39,19 @@ credentials: new FormGroup({
 
   Save(){
     console.log(this.reactiveForm);
+  }
 
+  addSkills( ){
+
+     this.skills.push(new FormControl(Validators.required));
   }
 
 
   get firstName(){
     return this.reactiveForm.get('firstName');
+  }
+
+  get skills(){
+    return this.reactiveForm.get('skills') as FormArray
   }
 }
